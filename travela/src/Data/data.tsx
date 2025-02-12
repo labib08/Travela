f44ea28eac3b81cff91e9016ce186cd83dea4fee
labib08 <iklabib49@gmail.com>
2025-02-12 17:02:27 +1100

    Added AI prompt

--- travela/src/Data/data.tsx
+++ travela/src/Data/data.tsx
@@ -63,5 +63,5 @@ export const SelectedBudgetOption:BudgetOption[] = [
         desc:"Don't worry about the cost",
         icon: '💳',
     },
-
 ]
+export const AI_PROMPT = "Generate Travel Plan for Location : {location}, for {totalDays} Days for {traveler} with a {budget}, give me Hotel options list with HotelName, Hotel address, Price, hotel image url, geo coordinates, rating, descriptions, and suggest itinerary with placeName, Place Details, Place Image Url, Geo Coordinates, ticket Pricing, time Travel each of the location for {totalDays} days with each day plan with best time to visit in JSON format."
