9e6abcefff78633d806f75b660c0fdea6756fc65
labib08 <iklabib49@gmail.com>
2025-02-12 14:00:14 +1100

    Formatted Navbar

--- travela/src/Components/Navbar/Navbar.tsx
+++ travela/src/Components/Navbar/Navbar.tsx
@@ -1,21 +1,22 @@
-import { Typography } from '@mui/material';
-import React from 'react';
-import logo from '../../assets/newLogo.png';
-import { ColorButton } from '../../Styles/Styles';
-import './Navbar.css';
-const Header:React.FC = () => {
-
+import { Typography } from "@mui/material";
+import React from "react";
+import logo from "../../assets/newLogo.png";
+import { ColorButton } from "../../Styles/Styles";
+import "./Navbar.css";
+const Header: React.FC = () => {
   return (
-    <div className='navbar'>
-      <div className='navbar-name'>
-      <img src={logo} alt ='' className='navbar-img'/>
-      <Typography variant='h3' style={{ fontWeight: 550 }}>Travela</Typography>
+    <div className="navbar">
+      <div className="navbar-name">
+        <img src={logo} alt="" className="navbar-img" />
+        <Typography variant="h3" style={{ fontWeight: 550 }}>
+          Travela
+        </Typography>
       </div>
       <div>
-      <ColorButton variant="contained">Sign In</ColorButton>
+        <ColorButton variant="contained">Sign In</ColorButton>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
