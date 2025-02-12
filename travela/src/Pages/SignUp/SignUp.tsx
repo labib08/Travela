0c1b2cd8820e240ffe8687e71a3a930825de7b29
labib08 <iklabib49@gmail.com>
2025-02-13 01:10:33 +1100

    Added the login design

--- travela/src/Pages/SignUp/SignUp.tsx
+++ travela/src/Pages/SignUp/SignUp.tsx
@@ -12,14 +12,6 @@ const SignUp = () => {
                 <button>Sign Up</button>
             </form>
         </div>
-        <div className="login">
-            <form>
-                <label htmlFor="chk" aria-hidden="true">Login</label>
-                <input type= "email" name="email" placeholder="Email" required/>
-                <input type= "Password" name="password" placeholder="Password" required/>
-                <button>Login</button>
-            </form>
-        </div>
     </div>
   )
 }
