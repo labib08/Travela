e12eafd3193c4f92ed8ae688ac6585e3eef4c2a8
labib08 <iklabib49@gmail.com>
2025-02-13 00:51:09 +1100

    Added styling for the SignUp part

--- travela/src/Pages/SignUp/SignUp.tsx
+++ travela/src/Pages/SignUp/SignUp.tsx
@@ -1,11 +1,11 @@
-
+import './SignUp.css'
 const SignUp = () => {
   return (
     <div className="signup">
         <input type="checkbox" id="chk" aria-hidden="true"/>
         <div className="signup-main">
             <form>
-                <label htmlFor = "chk" aria-hidden="true">Sign Up</label>
+                <label htmlFor = "chk" aria-hidden="true" className='signup-main-label'>Sign Up</label>
                 <input type= "text" name="txt" placeholder="Username" required/>
                 <input type= "email" name="email" placeholder="Email" required/>
                 <input type= "Password" name="password" placeholder="Password" required/>
