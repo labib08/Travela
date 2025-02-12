8179f0a268925ffa1c4e7a3249669420b6b0098c
labib08 <iklabib49@gmail.com>
2025-02-12 13:59:48 +1100

    Formatted Header

--- travela/src/Components/Header/Header.tsx
+++ travela/src/Components/Header/Header.tsx
@@ -1,29 +1,37 @@
-import { Button, Typography } from '@mui/material';
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Header.css';
+import { Button, Typography } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
 const Header: React.FC = () => {
-
   return (
-    <div className='header'>
+    <div className="header">
+      <Typography
+        variant="h2"
+        sx={{
+          mt: "50px",
+          textAlign: "center",
+          fontWeight: "650",
+          fontSize: "52px",
+        }}
+      >
+        <span className="header-ai">
+          {" "}
+          Discover Your Next Adventure with Ai:{" "}
+        </span>
+        Personalized Itineraries at Your Fingertips
+      </Typography>
 
-        <Typography variant='h2'
-          sx={{
-            mt: '50px',
-            textAlign: 'center',
-            fontWeight: '650',
-            fontSize: '52px'
-          }}>
-          <span className='header-ai'>  Discover Your Next Adventure with Ai: </span>
-             Personalized Itineraries at Your Fingertips
-        </Typography>
-
-        <Typography variant='h5' sx={{textAlign: 'center'}}>Your personal trip planner and travel curator, creating custom itineraries tailored to your interest and budget</Typography>
-        <Link to = '/create-trip'>
-        <Button variant="contained" className='header-button'>Get Started, It's Free</Button>
-        </Link>
+      <Typography variant="h5" sx={{ textAlign: "center" }}>
+        Your personal trip planner and travel curator, creating custom
+        itineraries tailored to your interest and budget
+      </Typography>
+      <Link to="/create-trip">
+        <Button variant="contained" className="header-button">
+          Get Started, It's Free
+        </Button>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
