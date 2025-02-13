81caf3d2660e9e74b0ee9a2966582f25fb2e03d4
labib08 <iklabib49@gmail.com>
2025-02-13 20:05:38 +1100

    Removed username from Sign up page

--- travela/src/Pages/SignUp/SignUp.tsx
+++ travela/src/Pages/SignUp/SignUp.tsx
@@ -7,7 +7,6 @@ const SignUp = () => {
         <div className="signup-main">
             <form>
                 <label htmlFor = "chk" aria-hidden="true" className='signup-main-label'>Sign Up</label>
-                <input type= "text" name="txt" placeholder="Username" required/>
                 <input type= "email" name="email" placeholder="Email" required/>
                 <input type= "Password" name="password" placeholder="Password" required/>
                 <button className="signup-button">Sign up</button>
