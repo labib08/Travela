afa78a11bc4a97a71be9a4dc8ec43bbdf2c96596
labib08 <iklabib49@gmail.com>
2025-02-14 15:29:04 +1100

    Added formData for signup

--- travela/src/Pages/SignUp/SignUp.tsx
+++ travela/src/Pages/SignUp/SignUp.tsx
@@ -1,25 +1,43 @@
+import { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
 import './SignUp.css';
+interface FormData {
+    email: string;
+    password: string;
+  }
 const SignUp = () => {
+    const [formData, setFormData] = useState<FormData>({
+        email: '',
+        password: ''
+      })
+      const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target;
+        setFormData({...formData, [name]: value});
+      }
+      const handleSubmit =(e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(formData);
+      }
   return (
     <div className="signup">
         <input type="checkbox" id="chk" aria-hidden="true"/>
         <div className="signup-main">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label htmlFor = "chk" aria-hidden="true" className='signup-main-label'>Sign Up</label>
-                <input type= "email" name="email" placeholder="Email" required/>
-                <input type= "Password" name="password" placeholder="Password" required/>
+                <input type= "email" name="email" placeholder="Email" onChange={handleChange} required/>
+                <input type= "Password" name="password" placeholder="Password" onChange={handleChange} required/>
                 <button className="signup-button">Sign up</button>
                 <div className="separator-container">
                     <div className="separator-line"></div>
                     <div className="separator-text">Or Continue With</div>
                     <div className="separator-line"></div>
                 </div>
-                <button className="google-signup">
+                <button className="google-signup" >
                     <FcGoogle className="google-logo" />
                     <span>Sign up with Google</span>
                 </button>
-                <p>Already have an account? <span className="signup-span"> Login </span></p>
+                <p>Already have an account? <span className="signup-span"> <Link to ="/login" className="signup-link"> Login </Link> </span></p>
             </form>
 
         </div>
