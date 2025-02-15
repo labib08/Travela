22ce1893a2f283fc82c02ce681f585445bd9b170
labib08 <iklabib49@gmail.com>
2025-02-15 17:28:09 +1100

    Initialized the database

---
+++ backend/config/database.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE travela;
+
+CREATE TABLE "user"(
+    user_id SERIAL PRIMARY KEY,
+    name VARCHAR(200) NOT NULL,
+    email VARCHAR(200) UNIQUE NOT NULL,
+    password TEXT NOT NULL
+);
