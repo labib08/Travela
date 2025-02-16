5cd4724c6e791049131c85b9b4ee99c424e81323
labib08 <iklabib49@gmail.com>
2025-02-16 16:24:30 +1100

    Completed the login backend and frontend

--- travela/src/Pages/Login/Login.tsx
+++ travela/src/Pages/Login/Login.tsx
@@ -1,11 +1,22 @@
+import { Alert, CircularProgress, Snackbar } from "@mui/material";
+import axios from "axios";
 import { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 interface FormData {
     email: string;
     password: string;
   }
 const Login = () => {
+    const url = "http://localhost:5000";
+    const [open, setOpen] = useState<boolean>(false);
+    const [isSuccess, setIsSuccess] = useState<boolean>();
+    const handleClose = () => {
+      setOpen(false);
+    };
+    const navigate = useNavigate();
+    const [loading, setLoading] = useState<boolean>(false);
+    const [message, setMessage] = useState<String>("");
     const [formData, setFormData] = useState<FormData>({
         email: '',
         password: ''
@@ -14,9 +25,28 @@ const Login = () => {
         const {name, value} = e.target;
         setFormData({...formData, [name]: value});
       }
-      const handleSubmit =(e: React.FormEvent<HTMLFormElement>) => {
+      const handleSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        console.log(formData);
+        setLoading(true);
+        const response = await axios.post(`${url}/api/user/login`, formData);
+        if (response.data.success) {
+
+          //toast.success("User registered successfully");
+          localStorage.clear();
+          localStorage.setItem("token", response.data.token);
+          setTimeout(() => {
+            setLoading(false);
+            navigate("/");
+          }, 5000);
+        }
+        else {
+          setIsSuccess(false)
+          setMessage(response.data.message);
+          console.log(message)
+          //toast.error(response.data.message);
+          setLoading(false);
+          setOpen(true);
+        }
       }
   return (
     <div className="signup">
@@ -26,7 +56,16 @@ const Login = () => {
                     <label htmlFor = "chk" aria-hidden="true" className='signup-main-label' >Login</label>
                     <input type= "email" name="email" placeholder="Email" onChange={handleChange} required/>
                     <input type= "Password" name="password" placeholder="Password" onChange={handleChange} required/>
-                    <button className="signup-button">Login</button>
+                    <button className="signup-button" disabled={loading} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                    {loading ? (
+                      <>
+                        <CircularProgress size={24} color="inherit" style={{ marginRight: 8 }} />
+                        Logging in
+                      </>
+                    ) : (
+                      "Sign Up"
+                    )}
+                  </button>
                     <div className="separator-container">
                         <div className="separator-line"></div>
                         <div className="separator-text">Or Continue With</div>
@@ -38,7 +77,35 @@ const Login = () => {
                     </button>
                     <p>Don't have an account? <span className="signup-span"> <Link to ="/signup" className="signup-link">Sign up </Link> </span></p>
                 </form>
-
+                {isSuccess ? (
+                <Snackbar
+                  open={open}
+                  autoHideDuration={5000}
+                  onClose={handleClose}
+                  anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                >
+                  <Alert
+                    onClose={handleClose}
+                    severity="success"
+                  >
+                    {message}
+                  </Alert>
+                </Snackbar>
+              ) : (
+                <Snackbar
+                  open={open}
+                  autoHideDuration={5000}
+                  onClose={handleClose}
+                  anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                >
+                  <Alert
+                    onClose={handleClose}
+                    severity="error"
+                  >
+                    {message}
+                  </Alert>
+                </Snackbar>
+              )}
             </div>
         </div>
   )
