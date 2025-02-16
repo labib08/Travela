6ec6664c5c954eac268b21d22ee50a1b78f1cc06
labib08 <iklabib49@gmail.com>
2025-02-16 15:23:46 +1100

    Completed the signup page

--- travela/src/Pages/SignUp/SignUp.tsx
+++ travela/src/Pages/SignUp/SignUp.tsx
@@ -1,8 +1,9 @@
+import { Alert, CircularProgress, Snackbar } from "@mui/material";
 import axios from "axios";
 import { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
 import { Link, useNavigate } from "react-router-dom";
-import { toast, ToastContainer } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import './SignUp.css';
 interface FormData {
     name: string;
@@ -11,7 +12,14 @@ interface FormData {
   }
 const SignUp = () => {
     const url = "http://localhost:5000";
+    const [open, setOpen] = useState<boolean>(false);
+    const [isSuccess, setIsSuccess] = useState<boolean>();
+    const handleClose = () => {
+      setOpen(false);
+    };
     const navigate = useNavigate();
+    const [loading, setLoading] = useState<boolean>(false);
+    const [message, setMessage] = useState<String>("");
     const [formData, setFormData] = useState<FormData>({
         name: '',
         email: '',
@@ -23,17 +31,28 @@ const SignUp = () => {
       }
       const handleSubmit =async(e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-
+        setLoading(true);
         const response = await axios.post(`${url}/api/user/create`, formData);
+
         if (response.data.success) {
           console.log("yay");
-          toast.success("User registered successfully");
+          setIsSuccess(true);
+          setOpen(true);
+          setMessage("User registered successfully");
+          //toast.success("User registered successfully");
           localStorage.clear();
           localStorage.setItem("token", response.data.token);
-          navigate('/');
+          setTimeout(() => {
+            setLoading(false);
+            navigate("/");
+          }, 5000);
+
         }
         else {
-          toast.error(response.data.message);
+          setIsSuccess(false)
+          setMessage(response.data.message);
+          //toast.error(response.data.message);
+          setLoading(false);
         }
       }
   return (
@@ -46,7 +65,9 @@ const SignUp = () => {
                 <input type= "text" name="name" placeholder="Name" onChange={handleChange} required/>
                 <input type= "email" name="email" placeholder="Email" onChange={handleChange} required/>
                 <input type= "Password" name="password" placeholder="Password" onChange={handleChange} required/>
-                <button className="signup-button">Sign up</button>
+                <button className="signup-button" disabled={loading}>
+                  {loading ? <CircularProgress size={24} color="inherit" /> : "Sign Up"}
+                </button>
                 <div className="separator-container">
                     <div className="separator-line"></div>
                     <div className="separator-text">Or Continue With</div>
@@ -58,7 +79,35 @@ const SignUp = () => {
                 </button>
                 <p>Already have an account? <span className="signup-span"> <Link to ="/login" className="signup-link"> Login </Link> </span></p>
             </form>
-
+            {isSuccess ? (
+            <Snackbar
+              open={open}
+              autoHideDuration={5000}
+              onClose={handleClose}
+              anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+            >
+              <Alert
+                onClose={handleClose}
+                severity="success"
+              >
+                {message}
+              </Alert>
+            </Snackbar>
+          ) : (
+            <Snackbar
+              open={open}
+              autoHideDuration={5000}
+              onClose={handleClose}
+              anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+            >
+              <Alert
+                onClose={handleClose}
+                severity="error"
+              >
+                {message}
+              </Alert>
+            </Snackbar>
+          )}
         </div>
     </div>
   )
