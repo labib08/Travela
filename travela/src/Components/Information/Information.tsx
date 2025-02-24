612b8b265c3402dc2760033b1cee96e4d462333b
labib08 <iklabib49@gmail.com>
2025-02-24 23:03:53 +1100

    Added dummy image

--- travela/src/Components/Information/Information.tsx
+++ travela/src/Components/Information/Information.tsx
@@ -1,13 +1,15 @@
 import Trip from '../../Types/tripType.ts';
+import planeImage from '../../assets/dummy.jpeg';
 import './information.css';
-
 interface Props {
     trip: Trip;
 }
 
 const Information:React.FC<Props> = ({trip}) => {
   return (
-    <div>Information</div>
+    <div>
+        <img src= {planeImage} alt = "" />
+    </div>
   )
 }
 
