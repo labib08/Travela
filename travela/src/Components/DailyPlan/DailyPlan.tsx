2191c002fd8e20a723fb8cbf91b3c73333704b92
labib08 <iklabib49@gmail.com>
2025-03-10 01:10:20 +1100

    Added the daily itinerary for trips

--- travela/src/Components/DailyPlan/DailyPlan.tsx
+++ travela/src/Components/DailyPlan/DailyPlan.tsx
@@ -1,6 +1,7 @@
 import Trip from '@/Types/tripType';
 import { Grid2, Typography } from '@mui/material';
 import planeImage from '../../assets/dummy5 copy 2.png';
+//import timeImg from "../../assets/iconmonstr-clock-thin.svg";
 import './DailyPlan.css';
 
 interface Props {
@@ -17,24 +18,25 @@ const DailyPlan:React.FC<Props> = ({savedTrip, mode}) => {
             const formattedDay = dayKey.replace(/day(\d+)/i, (_, num) => `Day ${num.charAt(0).toUpperCase() + num.slice(1)}`);
             return (
               <div key={index}>
-                <Typography variant='h2' sx={{ fontWeight: 'bold', fontSize: '1.5rem', marginTop: '20px' }}>
+                <Typography variant='h2' sx={{ fontWeight: 'bold', fontSize: '1.5rem', marginTop: '30px' }}>
                   {formattedDay}: {item.theme}
                 </Typography>
-                <Grid2 container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                <Grid2 container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} >
                   {item.places.map((place, i) => (
-                    <Grid2 size={6}>
+                    <Grid2 size={6} >
                     <div key={i} className='daily-plan-places'>
-                      <Typography variant='body1' sx={{fontWeight: 'bold'}} > {place.bestTimeToVisit} </Typography>
+                      <Typography variant='body1' sx={{fontWeight: 'bold'}} > {place.bestTimeToVisit.split(")")[0] + ")"} </Typography>
                       <div className={`daily-plan-places-desc ${mode === 'dark' ? 'dark-mode' : 'light-mode'}`}>
                         <img src={planeImage} alt='' className='daily-plan-places-img'/>
                         <div>
-                            <Typography variant='body1' sx = {{fontWeight: 'bold'}}>{place.placeName}</Typography>
+                            <Typography variant='h2' sx = {{fontWeight: 'bold', fontSize: '1.3rem', marginTop: '20px'}}>{place.placeName}</Typography>
                             <Typography
                                 variant="body1"
-                                sx={{ color: mode === "dark" ? "#cccccc" : "#4b555f",}}
+                                sx={{fontSize: '0.8rem', color: mode === "dark" ? "#cccccc" : "#4b555f", marginTop: '5px'}}
                                 >
                                 {place.placeDetails}
-                                </Typography>
+                            </Typography>
+                            <Typography variant='h2' sx = {{fontWeight: '500', fontSize: '0.8rem', marginTop: '5px'}}> {place.timeToTravel} </Typography>
                         </div>
                       </div>
 
